d2382ad27b97a7101eef96a6810c9329500f550d
Alejandro Santiago <ajsantiago@uninorte.edu.co>
2019-05-27 16:34:31 -0500

    changes

--- public/js/game.js
+++ public/js/game.js
@@ -55,7 +55,7 @@ function create() {
   scene = this;
 
   var pared = this.add.image(500, 300, 'pared');
-  this.physics.world.setBounds(320, 50, 366, 510)
+  this.physics.world.setBounds(350, 50, 396, 510)
   ball_lauched = false;
   ball_velocity = 250;
 
